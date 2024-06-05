dbd425cbd65e991d417f8f6b1fd491b6de7991e5
Jan Poonthong <janpoonthong628@gmail.com>
2024-06-05 16:57:48 +0700

    feat: add more case converter

--- title_convert.js
+++ title_convert.js
@@ -1,11 +1,35 @@
-function titleConvert(args) {
+#!/usr/bin/env node
+
+function noSpaceConvert(args) {
   let modifiedTitle = args.replace(/\W+/g, "_");
   return modifiedTitle;
 }
 
+function titleCaseConvert(args) {
+  let modifiedTitle = args.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+  return modifiedTitle;
+}
+
+function sentenceCaseConvert(args) {
+  let modifiedTitle =
+    args.charAt(0).toUpperCase() + args.substr(1).toLowerCase();
+  return modifiedTitle;
+}
+
 function main() {
-  const args = process.argv.slice(2);
-  const title = titleConvert(args[0].toLowerCase());
+  const args = require("args-parser")(process.argv);
+  let title;
+
+  if (args.nospace) {
+    title = noSpaceConvert(args.nospace.toLowerCase());
+  } else if (args.titlecase) {
+    title = titleCaseConvert(args.titlecase);
+  } else if (args.sentencecase) {
+    title = sentenceCaseConvert(args.sentencecase);
+  }
+
   console.log(title);
 }
 
